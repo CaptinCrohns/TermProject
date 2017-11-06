697868e2315d72eff51148e051a9eb09122629f1
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-11-06 16:04:06 +0000

    Added the association between category and products

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Product do
 
-permit_params :name, :price, :description, :quantity
+permit_params :name, :price, :description, :quantity, :category_id
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  belongs_to :category  # There is a category id in the product table
+
   validates :name, presence: true
 end
