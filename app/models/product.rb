3c52dafa68ab2f2d372e281c232f7e29f2fc79a1
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-11-15 03:51:22 +0000

    Resolved Heroku Issue, updated development.rb to reload server pages quicker, created header partial

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category  # There is a category id in the product table
-
   validates :description, presence: true
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
