fdf885cab55d5792a61991991b5ec11df002cbda
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-11-16 16:20:05 +0000

    Added search feature for products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   # validates :name, presence: true
+  def self.search(search)
+    where("name LIKE ? OR description LIKE ? ", "%#{search}%", "%#{search}%") 
+  end
+
 end
