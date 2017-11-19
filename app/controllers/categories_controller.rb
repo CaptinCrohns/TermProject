0b8f1fc978961ffd6c3441db490a0d61d54b747a
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-11-19 16:59:20 +0000

    Fixed up categories to display category products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,9 @@
 class CategoriesController < ApplicationController
   def index
       @categories = Category.all
-
   end
 
   def show
       @category = Category.find(params[:number].to_i)
   end
-  
 end
