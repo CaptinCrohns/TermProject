c43987005b92eaaec63758ae2940f4fa02a0b2bc
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-11-21 02:26:26 +0000

    Fixed routing and links for categories, added before_action in the application controller to autopopulate category links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+before_action :load_sidr_categories, only: [:index,:show, :shop]
+
+  def load_sidr_categories
+
+    @categories = Category.all
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
 
   get 'pages/about'
    get 'products/', to: 'products#shop', as: 'all_products'
-   get 'products/:number', to: 'products#show', as: 'product'
+   get 'products/:number', to: 'products#show', as: 'product', number: /\d+/
   get 'products/featured', to: 'products#featured', as: 'featured'
 
    get  'categories', to: 'categories#index', as: 'all_categories'
-    get  'categories/:number', to: 'categories#show', as: 'category'
+    get  'categories/:number', to: 'categories#show', as: 'category', number: /\d+/
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
