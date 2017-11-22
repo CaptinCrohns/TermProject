1bc4d11dd284180ef99e7da60b8f5788200098fc
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-11-22 00:37:03 +0000

    Created page model and gave active admin permissions and added route.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121155048) do
+ActiveRecord::Schema.define(version: 20171122001924) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20171121155048) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
