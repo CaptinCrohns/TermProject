5b79a0fe790ec2114e752cb0828ac3608db79c7e
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-12-06 03:38:08 +0000

    Fixed several links, attempted uninstall of atom, beautify, rbeautify.  Attempted to use rubocop to remove errors and fix spacing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-before_action :load_sidr_categories, only: [:index,:show, :shop, :about, :contact, :new, :create]
+  before_action :load_sidr_categories,
+         only: [:index, :show, :shop, :about, :contact, :new, :create]
   def load_sidr_categories
-
     @categories = Category.all
-
   end
-
-
 end
