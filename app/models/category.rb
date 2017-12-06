736b9c7c0d942ff4c508f392caf2d82e4b091100
Tyler Syrowitz <tsyrowitz@gmail.com>
2017-12-06 04:20:29 +0000

    Ran rubocop autofix for models directory and fixed remaining major errors.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,4 @@
 class Category < ApplicationRecord
-has_many :products
+  has_many :products
   validates :name, presence: true
-
-  # def self.search(search)
-  #   where("name LIKE ? ", "%#{search}%")
-  # end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,6 @@
 class Order < ApplicationRecord
   has_many :product_items, dependent: :destroy
   validates :name, :email, :address, :city, :province, presence: true
-
   def add_product_items_from_cart(cart)
     cart.product_items.each do |item|
       item.cart_id = nil
@@ -10,6 +9,5 @@ class Order < ApplicationRecord
   end
   def total_price
     product_items.map(&:total_price).sum
-
   end
 end
